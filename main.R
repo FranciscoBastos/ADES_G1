42ddc1ca210f273f3d38a13a9fbb7904e93cf174
Francisco Bastos <1191453@isep.ipp.pt>
2022-03-10 10:51:14 +0000

    Update main.R

--- main.R
+++ main.R
@@ -1,1 +1,21 @@
-read.csv("data/dev.csv")
+library("psych")
+library("corrplot")
+library("rpart")
+library("neuralnet")
+
+################################Load data#######################################
+
+dataCSV <- read.csv("data/dev.csv")
+
+############################Primary analysis####################################
+
+summary(dataCSV)
+head(dataCSV)
+tail(dataCSV)
+str(dataCSV)
+dim(dataCSV)
+
+
+
+
+
