226c6dc8d42ab0adaad7704d5775ce8a0cd2551f
Francisco Bastos <1191453@isep.ipp.pt>
2022-03-16 11:36:23 +0000

    Update main.R

--- main.R
+++ main.R
@@ -2,12 +2,13 @@ library("psych")
 library("corrplot")
 library("rpart")
 library("neuralnet")
+library("dplyr")
 
 ################################Load data#######################################
 
 dataCSV <- read.csv("data/dev.csv")
 
-############################Primary analysis####################################
+##############################Primary analysis##################################
 
 summary(dataCSV)
 head(dataCSV)
@@ -15,6 +16,21 @@ tail(dataCSV)
 str(dataCSV)
 dim(dataCSV)
 
+set.seed(2987465)
+dataCSV <- na.omit(dataCSV)
+dataCSV <- as.numeric(dataCSV)
+
+dataCSV[sample.int(nrow(dataCSV), 1000), ]
+
+############################K-Means clustering algorithm########################
+
+# Create a vector for analysis with only the first 1000 columns of the data-set.
+# Also we removed the 1 to the 3 columns and the last - because they had 
+# non numerical values. 
+tem.dataCSV <- dataCSV[1:1000,-c(1:3,ncol(dataCSV))]
+
+# K-Means algorithm from the simplified dataset.
+kmeans(tem.dataCSV, centers=2)
 
 
 
