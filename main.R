df4e70f2c1369dc13a84315b912223775ce0201e
Francisco Bastos <1191453@isep.ipp.pt>
2022-03-16 11:38:22 +0000

    Update main.R

--- main.R
+++ main.R
@@ -23,13 +23,15 @@ dataCSV <- as.numeric(dataCSV)
 dataCSV[sample.int(nrow(dataCSV), 1000), ]
 
 ############################K-Means clustering algorithm########################
-
+# For more information, if there is an error go-to:
+# https://data-hacks.com/r-error-do_one-nmeth-na-nan-inf-foreign-function-call-arg-1
+################################################################################
 # Create a vector for analysis with only the first 1000 columns of the data-set.
 # Also we removed the 1 to the 3 columns and the last - because they had 
 # non numerical values. 
 tem.dataCSV <- dataCSV[1:1000,-c(1:3,ncol(dataCSV))]
 
-# K-Means algorithm from the simplified dataset.
+# K-Means algorithm from the simplified data-set.
 kmeans(tem.dataCSV, centers=2)
 
 
