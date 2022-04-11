505c048c90d0d9d39c91dc7d37a12259857b534e
Francisco Bastos <1191453@isep.ipp.pt>
2022-04-11 19:16:32 +0100

    Update on predictions algorithms

--- main.R
+++ main.R
@@ -1,3 +1,14 @@
+install.packages('abind')
+install.packages('zoo')
+install.packages('xts')
+install.packages('quantmod')
+install.packages('ROCR')
+install.packages("DMwR")
+install.packages("smotefamily")
+install.packages('caret')
+
+library("caret")
+library("ROCR")
 library("psych")
 library("rpart")
 library("dplyr")
@@ -10,14 +21,11 @@ library("rpart.plot")
 library("neuralnet")
 library("party")
 library("partykit")
-library("DMwR2")
-install.packages('abind')
-install.packages('zoo')
-install.packages('xts')
-install.packages('quantmod')
-install.packages('ROCR')
-install.packages("DMwR")
 library("DMwR")
+library("DMwR2")
+library("smotefamily")
+
+suppressPackageStartupMessages(c(library(caret),library(corrplot),library(smotefamily)))
 
 ####################################HELPS#######################################
 # https://github.com/sed-inf-u-szeged/OpenStaticAnalyzer
@@ -65,73 +73,36 @@ tem.dataCSV.test <- tem.dataCSV[-index,]
 dim(tem.dataCSV.train)
 dim(tem.dataCSV.test)
 
+tem.dataCSV.train <- na.omit(tem.dataCSV.train)
+tem.dataCSV.test <- na.omit(tem.dataCSV.test)
+
 #################################Decision tree##################################
-output.tree <- rpart(
-  bugs ~ ., 
-  data = tem.dataCSV.train,
-  method = "class")
+dt <- rpart( bugs ~ .,
+             data = tem.dataCSV.train,
+             method = "class")
 
-print(output.tree)
+dt.preds <- predict(dt, tem.dataCSV.train, type = "class") # class
+dt.pred.probs <- predict(dt, tem.dataCSV.train, type = "prob") # ... and probabilities
+dt.pred.probs
 
+# make predictions for test data
+dt.preds <- predict(dt, tem.dataCSV.test, type="class") # class
 
-# Prediction
-predict.bug <- predict(output.tree, tem.dataCSV.test, type = 'prob')
-predict.bug
+# compute confusion matrix
+cm.dt <- table(dt.preds, tem.dataCSV.test$bugs)
+cm.dt
 
-# Turn prediction data into a data.frame
-df.predict.bug <- data.frame(predict.bug)
+accuracy <- sum(diag(cm.dt)) / sum(cm.dt)
+accuracy
+error.dt.test <- 1 - sum(diag(cm.dt)) / sum(cm.dt)
+error.dt.test
 
-# Eliminate the first column because it is the classes that do not have bugs
-df.predict.bug <- df.predict.bug[,-c(1)]
-df.predict.bug
+###################################### ROC #####################################
+# TODO ROC NOT WORKING - FIX LATER, ERROR MAYBE ON THE PREDICTION ARGUMENTS FEED
+roc.stats <- prediction(dt.preds[, "1"], tem.dataCSV.train$bugs)
+roc <- performance(roc.stats, "tpr", "fpr")
 
-# Display information as a table
-table_bug <- table(tem.dataCSV.test$bugs, df.predict.bug)
-table_bug
-
-# Confusion matrix 
-m.conf<-table(tem.dataCSV.test$bugs, df.predict.bug)
-
-print(m.conf)
-
-accuracy <- sum( diag(m.conf)) / sum (m.conf)
-
-sprintf("%f is the algorithm accuracy", accuracy)
-
-# Train the model with different samples to see if the accuracy changes
-set.seed(2987465)
-
-accuracy <- c()
-
-for (i in 1:10) {
-  index <- sample(1:nrow(tem.dataCSV), 0.7 * nrow(tem.dataCSV))
-  
-  data_train <- tem.dataCSV[index,]
-  data_test <- tem.dataCSV[-index,]
-  
-  output.tree <- rpart(
-    bugs ~ ., 
-    data = tem.dataCSV.train,
-    method = "class")
-  
-  predict.bug <- predict(output.tree, data_test, type = "prob")
-  # Turn prediction data into a data.frame
-  df.rpart_pred <- data.frame(predict.bug)
-  # Eliminate the first column because it is the classes that do not have bugs
-  df.rpart_pred <- df.rpart_pred[,-c(1)]
-  df.rpart_pred
-  
-  m.conf <- table(data_test$bugs, df.rpart_pred)
-  
-  accuracy <- c(accuracy, sum( diag(m.conf)) / sum (m.conf))
-  
-}
-
-mean <- mean(accuracy)
-sd <- sd(accuracy)
-
-sprintf("%f is the mean accuracy", mean)
-sprintf("%f is the standard deviation", sd)
+################################################################################
 
 # ATENTION: YOU NEED TO USE THE COMPETION DATA, THE ALL DATASET.
 #           YOU CANNOT DEVIDE THE DATASET INTO TRAINING AND TESTING.
@@ -141,6 +112,9 @@ sprintf("%f is the standard deviation", sd)
 # TO SELECT A CORRECT BETTER IMPUT DATA FRAME
 # NOTE: THE ACCURACY IS 0.618431 WITH THE CLASSIFICATION TREE!
 #       NEXT TIME TRY TO IMPROVE.
+#
+# Cannot calculate confusion matrix, error and accuracy
+# because the bugs column does not exist.
 
 # Export data set to more or less Kaggle format
 
@@ -151,27 +125,17 @@ tem.dataCSVComp <- dataCSVComp[-c(1:7)]
 #######################Decision tree with competition data######################
 
 # Prediction
-predict.bug <- predict(output.tree, tem.dataCSVComp, type = 'prob')
+predict.bug <- predict(dt, tem.dataCSVComp, type = 'prob')
 predict.bug
+
+# make predictions for training data
+dt.preds <- predict(dt, tem.dataCSVComp, type="class") # class
 
 # Turn prediction data into a data.frame
 df.predict.bug <- data.frame(predict.bug)
 # Eliminate the first column because it is the classes that do not have bugs
 df.predict.bug <- df.predict.bug[,-c(1)]
 df.predict.bug
-
-# Display information as a table
-table_bug <- table(tem.dataCSVComp$Bugs, df.predict.bug)
-table_bug
-
-# Confusion matrix 
-m.conf<-table(tem.dataCSVComp$Bugs, df.predict.bug)
-
-print(m.conf)
-
-accuracy <- sum( diag(m.conf)) / sum (m.conf)
-
-print(accuracy)
 
 predict.bug <- predict.bug[,-c(-2)]
 predict.bug
@@ -198,10 +162,11 @@ write.csv(predict.bug, "submissions/decision_tree_formated.csv",
 # https://www.r-bloggers.com/2021/05/class-imbalance-handling-imbalanced-data-in-r/ 
 # 
 ################################################################################
+##################### Analyze our data set and fix unbalanced ##################
 
 if(!require('ROSE')) {
-  install.packages('ROSE')
-  library('ROSE')
+        install.packages('ROSE')
+        library('ROSE')
 }
 
 suppressMessages(library(plm)) # to remove note about dependencies
@@ -246,15 +211,31 @@ tem.dataCSV.test <- tem.dataCSV.balanced[-index,]
 dim(tem.dataCSV.train)
 dim(tem.dataCSV.test)
 
-both <- ovun.sample(bugs~., data=tem.dataCSV.train, method = "both",
-                    p = 0.5,
-                    seed = 222)$data
-table(both$bugs)
+under <- ovun.sample(bugs~., data=tem.dataCSV.train, method = "under")$data
+table(under$bugs)
 
-# TODO FIX THE SMOTE AND ROSE ERRORS
-smote = SMOTE(bugs ~.,
-              target = tem.dataCSV.train$bugs,
-              K = 3,
-              dup_size = 3)
+# Visualize the data
+barplot(prop.table(table(under$bugs)),
+        col = rainbow(2),
+        ylim = c(0, 1),
+        main = "Class Distribution")
 
-table(rose$bugs)
+############################ Prediction with the under data ####################
+
+dt <- rpart( bugs ~ .,
+             data = under,
+             method = "class")
+
+dt.preds <- predict(dt, under, type = "class") # class
+dt.pred.probs <- predict(dt, under, type = "prob") # ... and probabilities
+dt.pred.probs
+
+# compute confusion matrix
+cm.dt <- table(dt.preds, under$bugs)
+cm.dt
+
+accuracy <- sum(diag(cm.dt)) / sum(cm.dt)
+accuracy
+error.dt.test <- 1 - sum(diag(cm.dt)) / sum(cm.dt)
+error.dt.test
+
