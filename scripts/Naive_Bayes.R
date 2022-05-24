e5e84c8bc4e5e8e011c034aa1edea64bbd1b2ba7
Francisco Bastos <1191453@isep.ipp.pt>
2022-05-24 16:17:20 +0100

    Update on Naive Bayes

--- scripts/Naive_Bayes.R
+++ scripts/Naive_Bayes.R
@@ -70,7 +70,7 @@ tem.dataCSV.balanced.sample$bugs <-
 barplot(prop.table(table(tem.dataCSV.balanced.sample$bugs)),
         col = rainbow(2),
         ylim = c(0, 1),
-        main = "Class distribution")
+        main = "Bugs class distribution")
 
 ################################################################################
 set.seed(2987465)
@@ -83,8 +83,12 @@ parts = createDataPartition(tem.dataCSV.balanced.sample$bugs,
 
 tem.dataCSV.train = tem.dataCSV.balanced.sample[parts, ]
 tem.dataCSV.test = tem.dataCSV.balanced.sample[-parts, ]
+
 tem.dataCSV.train = tem.dataCSV.train[complete.cases(tem.dataCSV.train), ]
 tem.dataCSV.test = tem.dataCSV.test[complete.cases(tem.dataCSV.test), ]
+
+X_train = tem.dataCSV.train[,-ncol(tem.dataCSV.balanced.sample)]
+y_train = tem.dataCSV.train[,ncol(tem.dataCSV.balanced.sample)]
 
 dim(tem.dataCSV.train)
 dim(tem.dataCSV.test)
@@ -115,43 +119,55 @@ train.smote.both <-
               method = "both")$data
 table(train.smote.both$bugs)
 
-# train.smote.both.target <- tem.dataCSV.train.SMOTE
-
 # Visualize the data
 barplot(prop.table(table(train.smote.both$bugs)),
         col = rainbow(2),
         ylim = c(0, 1),
-        main = "Class distribution (SMOTE, Over, Under)")
+        main = "Bugs class distribution (SMOTE, Over, Under)")
 
 ################################################################################
 ############################# Feature selection ################################
-# Define the control using a naive bayes selection function
+# Define the control using a naive Bayes selection function
 # Change for other functions for classification problems, see the link bellow:
 # http://topepo.github.io/caret/recursive-feature-elimination.html#rfe
 # Run the RFE algorithm
 ################################################################################
-##################### Apply the naive bayes algorithm ##########################
+# Turn last column of the data set into a factor
+# To run the naive Bayes algorithm
+
+class(tem.dataCSV.train[,ncol(tem.dataCSV.balanced.sample)])
+
+tem.dataCSV.train[,ncol(tem.dataCSV.balanced.sample)] <- 
+  as.factor(tem.dataCSV.train[,ncol(tem.dataCSV.balanced.sample)]) 
+
+# SHOULD BE A FACTOR
+class(tem.dataCSV.train[,ncol(tem.dataCSV.balanced.sample)])
 ################################################################################
-model <- naive_bayes(bugs ~ NOI + RFC + CBO + WMC + 
-                       Coupling.Metric.Rules + JUnit.Rules + 
-                       Strict.Exception.Rules + NII + CBOI + LLOC +
-                       TLLOC + NA. + NOA + TNOS + NLE + TLOC + 
-                       Complexity.Metric.Rules + LOC + DIT +
-                       NL + NOS + WarningMajor + WarningMinor + 
-                       Unnecessary.and.Unused.Code.Rules + 
-                       WarningInfo + TNA + NLM + 
-                       Type.Resolution.Rules + Clone.Metric.Rules +
-                       PUA + NM + Documentation.Metric.Rules +
-                       Inheritance.Metric.Rules + LDC + NLA +
-                       LLDC + NG + TNLS + CI + Brace.Rules + 
-                       String.and.StringBuffer.Rules + CD +
-                       Cohesion.Metric.Rules + AD + Android.Rules +
-                       Basic.Rules + CC + CCL + CCO + CLC + 
-                       CLLC + CLOC + Clone.Implementation.Rules +
-                       Controversial.Rules + Design.Rules +
-                       DLOC + Empty.Code.Rules +
-                       Finalizer.Rules + Import.Statement.Rules +
-                       J2EE.Rules, 
+control <- rfeControl(functions = nbFuncs,
+                      method = "repeatedcv",
+                      number = 10,
+                      verbose = TRUE)
+
+subsets <- c(1:ncol(train.smote.both), 10, 15, 20, 25)
+################################################################################
+# Run the RFE algorithm
+################################################################################
+results <- rfe(x = X_train, 
+               y = y_train, 
+               sizes = subsets, 
+               rfeControl = control)
+# summarize the results
+print(results)
+# list the chosen features
+predictors(results)
+# plot the results
+plot(results, type=c("g", "o"))
+variables <- data.frame(results$optVariables) # STORE THE VARIABLES
+variables$results.optVariables
+
+##################### Apply the naive Bayes algorithm ##########################
+################################################################################
+model <- naive_bayes(bugs ~ RFC, 
                      data = train.smote.both,
                      usekernel = T) 
 plot(model)
@@ -182,14 +198,14 @@ misclassification_Traning
 
 
 ############################ Test data #########################################
-p2 <- predict(model, tem.dataCSV.test)
+p2 <- predict(model, tem.dataCSV.test, type = 'prob')
 
 cm2 <- table(p2, tem.dataCSV.test$bugs)
 cm2
 # Model Evaluation
 confusionMatrix(cm2)
 
-misclassification_Testing<-1-sum(diag(cm2))/sum(cm2)
+misclassification_Testing <- 1 - sum(diag(cm2))/sum(cm2)
 misclassification_Testing
 
 ## Misclassification is around 15%%.
@@ -203,7 +219,7 @@ dt.preds <- as.numeric(p2)
 roc.accuracy <- roc(tem.dataCSV.test$bugs, dt.preds)
 print(roc.accuracy)
 plot(roc.accuracy)
-# The area under the curve is 0.6726
+# The area under the curve is 0.7044
 
 ################################################################################
 dataCSVComp <- read.csv("data/comp.csv")
@@ -220,8 +236,8 @@ dataCSVComp <- subset(dataCSVComp, select=-c(ID, Parent, Component, Line,
 
 ####################### Naive Bayes with competition data ######################
 # Prediction
-predict.bug <- predict(model,data = tem.dataCSVComp, type = 'prob')
-predict.bug
+predict.bug <- predict(model, data = dataCSVComp, type = 'prob')
+predict.bug <- predict.bug[-c(1)]
 
 # Save the data into submission format.
 write.csv(predict.bug, "submissions/naive_bayes.csv")
